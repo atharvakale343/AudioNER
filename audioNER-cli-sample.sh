a42372a6691a528c41a9c35180e0f75f74efcb9f
anjus <anjuskumar1313@gmail.com>
2024-09-23 11:13:12 -0400

    Added more sample data

--- audioNER-cli-sample.sh
+++ audioNER-cli-sample.sh
@@ -4,5 +4,5 @@ python src/audioNER/app_server.py &
 
 sleep 3
 
-python src/audioNER/app_client.py --audio_files data/audio.mp3 data/news.mp3 data/georgewashington.mp3
+python src/audioNER/app_client.py --audio_files data/harrypotter.mp3 data/cybertruck.mp3 data/georgewashington.mp3
 
